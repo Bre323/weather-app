de0a988c4e02601230f676705e54e8af8d409ad9
Breno Rocha <brenopenarocha2@gmail.com>
2024-10-09 13:32:29 -0300

    webpack configurations with React

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import React from 'react';
+
 const App = () => {
     return (
-        <h1>Hello</h1>
-    )
-}
+        <>
+            <h1>Hello</h1>
+        </>
+    );
+};
 
 export default App;
